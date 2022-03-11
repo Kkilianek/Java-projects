e0004651df576693028cf89a24ffea047a83e224
Kacper Kilianek <100278714+Kkilianek@users.noreply.github.com>
2022-03-11 15:34:41 +0100

    Add files via upload

---
+++ collections/Zadanie1_USERPASSWORD.java
@@ -0,0 +1,85 @@
+package collections;
+
+import java.util.Hashtable;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Zadanie1_USERPASSWORD {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Map<String, String> creds = new Hashtable<>();
+        int wybor;
+        System.out.println("PROGRAM USERPASSWORD");
+        System.out.println("------------------------------");
+        System.out.println("Co chcesz zrobić?");
+        System.out.println("1. Dodać nowe dane do bazy");
+        System.out.println("2. Spojrzeć na dodane już dane");
+        System.out.println("3. Zamknąć program");
+        wybor = sc.nextInt();
+        try {
+            switch (wybor) {
+                case 1:
+                    credentialInsertion(creds);
+                case 2:
+                    checkingInserted(creds);
+                case 3:
+                    System.out.println("ZAMYKANIE PROGRAMU!");
+                    System.exit(0);
+
+            }
+        } catch (RuntimeException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    private static void checkingInserted(Map<String, String> creds) throws RuntimeException {
+        System.out.println("TRYB WYŚWIETLANIA WCZEŚNIEJ DODANYCH DANYCH");
+        System.out.println("-------------------------------------------");
+        if (creds.isEmpty()) {
+            throw new RuntimeException("BAZA DANYCH JEST PUSTA! URUCHOM PROGRAM PONOWNIE I DODAJ DANE!");
+        } else {
+            System.out.println("Podaj nazwę użytkownika, którą chcesz sprawdzić: ");
+            Scanner in = new Scanner(System.in);
+            String user = in.nextLine();
+            if (creds.containsKey(user)) {
+                System.out.print("TWOJE HASŁO TO: ");
+                System.out.println(creds.get(user));
+            } else {
+                System.out.println("Nie znaleziono podanego użytkownika");
+            }
+
+        }
+    }
+
+    private static void credentialInsertion(Map<String, String> creds) throws RuntimeException {
+        boolean go = true;
+        boolean qflag = false;
+        Scanner input = new Scanner(System.in);
+        while (!qflag && go) {
+            System.out.println("Podaj nazwę użytkownika: ");
+            String user = input.nextLine();
+            if (user.equalsIgnoreCase("q")) {
+                System.out.println("Podano niedozwolony znak WYJŚCIA Z PROGRAMU.");
+                System.out.println("PROGRAM ZAKOŃCZYŁ PRACĘ.");
+                qflag = true;
+                break;
+            } else {
+                System.out.println("Podaj hasło: ");
+                String password = input.nextLine();
+                creds.put(user, password);
+                System.out.println("Dane zostały podane poprawnie. Czy chcesz podać kolejne dane?(Y/N)");
+                String choose = input.nextLine();
+                if (choose.equalsIgnoreCase("y")) {
+                    System.out.println("Wybrałeś Y. Przejdź do dodania kolejnych danych.");
+                } else if (choose.equalsIgnoreCase("n")) {
+                    System.out.println("PROGRAM PRZECHODZI DO NASTĘPNEGO TRYBU PRACY!");
+                    go = false;
+                } else {
+                    throw new RuntimeException("Niedozwolony znak");
+                }
+            }
+        }
+    }
+}
---
+++ collections/Zadanie2_SKLEP.java
@@ -0,0 +1,95 @@
+package collections;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class Zadanie2_SKLEP {
+    public static void main(String[] args) throws IOException {
+        BufferedReader readernazw = null;
+        BufferedReader readercen = null;
+        BufferedReader readerliczb = null;
+        try {
+            int lines = 0; //liczba linii w plikach tekstowych dotyczących produktów
+            File pliknazwa = new File("Lab2\\src\\lab2\\produkty.txt");
+            File plikcena = new File("Lab2\\src\\lab2\\produkty_cena.txt");
+            File plikliczb = new File("Lab2\\src\\lab2\\produkty_liczby.txt");
+            BufferedReader temp = new BufferedReader(new FileReader(pliknazwa));
+            readercen = new BufferedReader(new FileReader(plikcena));
+            ArrayList<Produkt> listaProduktów = new ArrayList<>();
+            ArrayList<Klient> listaKlientów = new ArrayList<>();
+            LinkedList<Klient> kolejka = new LinkedList<>();
+            //Dodawanie przykładowych produktów
+            while (temp.readLine() != null) {
+                lines++;
+            }
+            temp.close();
+            readernazw = new BufferedReader(new FileReader(pliknazwa));
+            readerliczb = new BufferedReader(new FileReader(plikliczb));
+            String[] nazwy = new String[lines];
+            double[] ceny = new double[lines];
+            int[] liczby = new int[lines];
+            for (int i = 0; i < lines; i++) {
+                nazwy[i] = readernazw.readLine();
+//                System.out.println(nazwy[i]);
+            }
+            for (int i = 0; i < lines; i++) {
+                ceny[i] = Double.valueOf(readercen.readLine());
+//                System.out.println(ceny[i]);
+            }
+            for (int i = 0; i < lines; i++) {
+                liczby[i] = Integer.valueOf(readerliczb.readLine());
+//                System.out.println(liczby[i]);
+            }
+            for (int i = 0; i < ceny.length; i++) {
+                listaProduktów.add(new Produkt(nazwy[i], ceny[i],liczby[i]));
+//                System.out.println(listaProduktów.get(i));
+            }
+            //Dodawanie przykładowych klientów do sklepu
+            for (int i = 0; i < 15; i++) {
+                listaKlientów.add(new Klient());
+            }
+            // Klienci wybierają produkty
+            for (Klient klient : listaKlientów) {
+                for (int i = 0; i < listaProduktów.size(); i++) {
+                    int liczebnosc = listaProduktów.size() - 1 - ((int) (Math.random() * 16));
+                    klient.koszyk.add(listaProduktów.get(liczebnosc));
+                    listaProduktów.get(liczebnosc).liczbaSztukProduktu--;
+                }
+            }
+            //Dodawanie klientów do kolejki + Test różności ID klientów + dodanie tylko klientów, którzy mają niepusty koszyk
+            for (Klient klient : listaKlientów) {
+                if (!klient.koszyk.empty()) {
+                    kolejka.add(klient);
+                }
+            }
+            for (Klient klient : kolejka) {
+                System.out.println(klient.toString());
+            }
+            //Obsługiwanie klientów
+            System.out.println("OBSŁUGIWANIE KLIENTÓW");
+            System.out.println("----------------------------");
+            for (Klient klient : kolejka) {
+                System.out.println("KLIENT NR " + klient.iD + " MA DO ZAPŁATY " + klient.koszyk.getSum());
+            }
+            System.out.println("--------------------------------------------------");
+            System.out.println("DZIĘKUJEMY ZA SKORZYSTANIE Z USŁUG NASZEGO SKLEPU!");
+
+        } finally {
+            if (readernazw != null) {
+                readernazw.close();
+            }
+            if (readercen != null) {
+                readercen.close();
+
+            }
+            if (readerliczb != null) {
+                readerliczb.close();
+            }
+        }
+
+    }
+}
